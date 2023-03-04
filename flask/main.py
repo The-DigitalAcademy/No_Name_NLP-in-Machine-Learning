a17c30626ca03ad93274817c1400d7e29eb6cf48
Edwinhlaluko <85001187+Edwinhlaluko@users.noreply.github.com>
2023-03-04 22:11:30 +0200

    Fixed the flask

--- flask/main.py
+++ flask/main.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, render_template
 import joblib as jb 
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+
 
 model = jb.load('spams.joblib')
 
@@ -16,24 +25,31 @@ def index():
 
 def predict():
     # extract input features from the request data
+    spams = pd.read_csv("C:\\Users\\hlala\\Downloads\\spam.csv", encoding="latin-1")
     
-    data =  np.array([
-                      str(request.form['message'])
-                     ])
-    #Reshape data
-    data = data.reshape(1, -1)
-
-    # Make prediction
-    prediction = model.predict(data)[0]
-
-    if prediction == 1:
-        msg = 'spam'
-
-    else:
-        msg = 'not spam'
-
+    spams['label'] = spams['v1'].map({'ham': 0, 'spam': 1})
+    spams['message']=spams['v2']
+    spams.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4','v1','v2'], axis=1, inplace=True)
+    X = spams['message']
+    y = spams['label']
+   
+    # Extract Feature With CountVectorizer
+    cv = CountVectorizer()
+    X = cv.fit_transform(X)  # Fit the Data
+  
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    # Naive Bayes Classifier
+    clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+    clf.score(X_test, y_test)
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+        
     # return the prediction as a json object
-    return render_template('predict.html',prediction=prediction,message = msg)
+    return render_template('prediction.html',prediction=my_prediction)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8085)
--- flask/model.py
+++ flask/model.py
@@ -1,36 +1,71 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import classification_report
-import joblib as jb
+# import pandas as pd
+# import numpy as np
+# from sklearn.model_selection import train_test_split
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.naive_bayes import MultinomialNB
+# from sklearn.metrics import classification_report
+# import joblib as jb
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.svm import SVC
+# from sklearn.preprocessing import LabelEncoder
 
-#importing the csv
-spams = pd.read_csv("C:\\Users\\hlala\\Downloads\\spam.csv", encoding="latin-1")
-spams['label'] = spams['v1'].map({'ham': 0, 'spam': 1})
-spams['message']=spams['v2']
-spams.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4','v1','v2'], axis=1, inplace=True)
-#Balancing data
-ham=spams[spams["label"]==0]
-spam=spams[spams["label"]==1]
-ham=ham.sample(spam.shape[0])
-data=spam.append(ham,ignore_index=True)
 
-X=spams["message"]
-Y=spams["label"]
-#fit the model
-cv=CountVectorizer()
-#fit the Data
-X=cv.fit_transform(X)
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=42)
-#naive bayes classifier
-clf=MultinomialNB()
-clf.fit(X_train,Y_train)
-clf.score(X_test,Y_test)
-y_pred=clf.predict(X_test)
-print(classification_report(Y_test,y_pred))
+# #importing the csv
+# spams = pd.read_csv("C:\\Users\\hlala\\Downloads\\spam.csv", encoding="latin-1")
+# spams['label'] = spams['v1'].map({'ham': 0, 'spam': 1})
+# spams['message']=spams['v2']
+# spams.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4','v1','v2'], axis=1, inplace=True)
+# #Balancing data
+# ham=spams[spams["label"]==0]
+# spam=spams[spams["label"]==1]
+# ham=ham.sample(spam.shape[0])
+# data=spam.append(ham,ignore_index=True)
 
-filename = 'spams.joblib'
-jb.dump(clf,filename) 
-print('model saved successfully!')
+# tf_vec = TfidfVectorizer()
+
+# #naive = MultinomialNB()
+
+# SVM = SVC(C=1.0, kernel='linear', degree=3 , gamma='auto')
+
+# features = tf_vec.fit_transform(spams['message'])
+
+# X = features
+# y = spams['label']
+
+# # Fit the vectorizer to the text data
+# vectorizer.fit(text_data)
+
+# # Transform the text data to a bag-of-words representation
+# bow_data = vectorizer.transform(text_data)
+
+# # Print the resulting sparse matrix
+# print(bow_data)
+
+# cat_data = ["red", "blue", "green", "red", "blue"]
+
+# # Create the label encoder object
+# encoder = LabelEncoder()
+
+# # Fit the encoder to the categorical data
+# encoder.fit(cat_data)
+
+# # Transform the categorical data to a numeric format
+# num_data = encoder.transform(cat_data)
+
+# X=spams["message"]
+# Y=spams["label"]
+# #fit the model
+# cv=CountVectorizer()
+# #fit the Data
+# X=cv.fit_transform(X)
+# X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.25,random_state=42)
+# #naive bayes classifier
+# clf=MultinomialNB()
+# clf.fit(X_train,Y_train)
+# clf.score(X_test,Y_test)
+# y_pred=clf.predict(X_test)
+# print(classification_report(Y_test,y_pred))
+
+# filename = 'spams.joblib'
+# jb.dump(clf,filename) 
+# print('model saved successfully!')
